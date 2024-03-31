5d8af9ead527cc79b5d61c7c56aeb37cc1396e33
Joel Lorenz Boada <jlorenz@bdnplus.com>
2024-03-31 20:15:22 +0200

    Add air pollution component to display air quality

--- src/types/weatherData.ts
+++ src/types/weatherData.ts
@@ -1,11 +1,15 @@
 export interface WeatherData {
     location: {
         name: string;
+        localtime: string;
     };
     current: {
         temp_c: number;
         condition: {
             text: string;
+        };
+        air_quality: {
+            "gb-defra-index": number;
         };
     };
 }
